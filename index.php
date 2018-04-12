151d7d154f643b1f58940afde497609818f2d43c
John Emrick <emrickj248@comcast.net>
2018-04-12 14:32:52 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
-// Bootstrap Scrollspy XSLT theme.
+// Bootstrap 4 Vertical Button XSLT theme.
 
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
+
+if(isset($_GET['p'])) $p = $_GET['p'];
+  else $p="1";
+
+if(isset($_GET['w'])) $w = $_GET['w'];
+  else $w="1";
 
 // Load XML file
 $xml = new DOMDocument;
@@ -10,10 +16,12 @@ $xml->load('data/website.xml');
 
 // Load XSL file
 $xsl = new DOMDocument;
-$xsl->load('xslt/theme3.xsl');
+$xsl->load('xslt/theme1bs4.xsl');
 
 // Configure the transformer
 $proc = new XSLTProcessor;
+$proc->setParameter('', 'page', intval($p));
+$proc->setParameter('', 'website', intval($w));
 
 // Attach the xsl rules
 $proc->importStyleSheet($xsl);

1a0cf9794bff87dd491733da5712b3c112dd7d33
John Emrick <emrickj248@comcast.net>
2018-04-13 10:01:53 -0400

    Add files via upload
    Switching of websites is now done in this file.

--- index.php
+++ index.php
@@ -12,8 +12,13 @@ if(isset($_GET['w'])) $w = $_GET['w'];
 
 // Load XML file
 $xml = new DOMDocument;
-$xml->load('data/website.xml');
-
+if($w=="2") $xml->load('data/website2.xml');
+  else $xml->load('data/website.xml');
+$items = $xml->getElementsByTagName('contents');
+$contents = trim($items->item(intval($p)-1)->nodeValue);
+if($w=="2") $contents = str_replace('"?p=','"?w=2&amp;p=',$contents);
+$contents = str_replace("'","&#39;",$contents);
+  
 // Load XSL file
 $xsl = new DOMDocument;
 $xsl->load('xslt/theme1bs4.xsl');
@@ -22,6 +27,9 @@ $xsl->load('xslt/theme1bs4.xsl');
 $proc = new XSLTProcessor;
 $proc->setParameter('', 'page', intval($p));
 $proc->setParameter('', 'website', intval($w));
+$proc->setParameter('', 'contents', $contents);
+if($w=="2") $proc->setParameter('','wpm1','w=2&');
+if($w=="1") $proc->setParameter('','wpm2','w=2&');
 
 // Attach the xsl rules
 $proc->importStyleSheet($xsl);
